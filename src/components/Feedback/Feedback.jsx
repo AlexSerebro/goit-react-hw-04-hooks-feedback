5f8f2bfb5ce52dbdbe7425cd7dc21243addf299a
Aleksandr Serebro <aleksandrserebroz@gmail.com>
2022-05-19 23:19:06 +0300

    fedback hook refactor

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,36 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Section} from '../Section'
 import { Button } from '../Button';
 import { Statistics } from '../Statistics';
 
 
 
-class Feedback extends React.Component{
-  state={
-    good:0,
-    neutral:0,
-    bad:0,
-  }
-  handleClick = event =>{
-    console.log(event);
-      this.setState((prevState) => {
-      return {
-        [event]: prevState[event] + 1,
-      };
-    });
-  }
+export default function Feedback() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+    const handleClick = e =>{
+      if (e === 'good') { setGood(prevGood => prevGood + 1) }
+      if (e === 'bad') { setBad(prevBad => prevBad + 1) }
+      if(e==='neutral'){setNeutral(prevNeutral=>prevNeutral+1)}
+      
+    };
   
-  totalSumFeedback =()=> this.state.bad + this.state.neutral + this.state.good;
-  totalPositiveFeedback = (totalSumFeedback) => {
-    return Math.round((this.state.good/totalSumFeedback)*100)
+
+  const totalSumFeedback =()=>bad + neutral + good;
+  const totalPositiveFeedback = (totalSumFeedback) => {
+    return Math.round((good/totalSumFeedback)*100)
   }
-  
-  render() {
-    const { good, neutral, bad } = this.state;
-      return(
+
+  return(
       <div>
         <Section title='Please leave feedback'>
-          <Button buttonNames={['good', 'neutral', 'bad']} liaveFeesback={this.handleClick}/>
+          <Button buttonNames={['good', 'neutral', 'bad']} liaveFeesback={handleClick}/>
 
         </Section>
         <Section title='Statistics'>
@@ -38,15 +34,12 @@ class Feedback extends React.Component{
             good={good}
             neutral={neutral}
             bad={bad}
-            totalSumFeedback={this.totalSumFeedback}
-            totalPositiveFeedback={this.totalPositiveFeedback}
+            totalSumFeedback={totalSumFeedback}
+            totalPositiveFeedback={totalPositiveFeedback}
           />
         </Section>
         </div>
 
     )
-  }
-}
+};
 
-
-export default Feedback;
